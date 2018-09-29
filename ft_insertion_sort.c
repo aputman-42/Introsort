783dbd47cdc143974e96a26e2be35f5504b1e455
Alexis Putman <aputman@student.42.fr>
2018-09-29 17:14:38 +0200

    Add comment to insertion sort to detail how it works and expected performance

--- ft_insertion_sort.c
+++ ft_insertion_sort.c
@@ -12,6 +12,16 @@
 
 #include "libsort.h"
 
+
+/*
+ * For every value the insertion sort will save the current value and compare
+ * it to every previous value until it finds its final position and shifts the
+ * array to insert the saved value into position
+ *
+ * Best case performance:	O(n)	comparisons, O(1)	swaps (array sorted)
+ * Average performance:		O(n^2)	comparisons, O(n^2)	swaps
+ * Worst case performance:	O(n^2)	comparisons, O(n^2)	swaps (reverse sorted)
+ */
 void	ft_insertion_sort(int *arr, const size_t len)
 {
 	int		i;
